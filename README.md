ec4ff53ccbda7136cd524dd9e4563a532c25b714
Craig Rueda <craig@davesplace.cc>
2016-07-24 16:57:14 -0700

    Added stuff to readme / Added S3 support

--- README.md
+++ README.md
@@ -5,18 +5,34 @@
 In a nut shell, this is a simple Python script that spawns an SMTP server, accepting emails from any sender. Whenever an email is received, the contents are scanned and the first attached email is then uploaded to the slack channel of your choosing.
 
 ##Usage
-1. Install dependencies (logbook, slacker, argparse):
+1. Install dependencies (logbook, slacker, argparse, boto, pillow):
 ```Bash
 pip install argparse
 pip install logbook
 pip install slacker
+pip install boto
+pip install pillow
 ```
 2. Create a Slack bot, and grab the API key suppied [here](https://api.slack.com/bot-users)
 3. Run emailServer.py
 ```Bash
-python emailServer.py -c <Your slack channel> -p <SMTP port to listen on - defaults to 2500> -s <Your Bot's Slack API key>
+python emailServer.py -c <Your config file>
 ```
 Example: 
 ```Bash
-python emailServer.py -p 2800 -s erlkter-ert534e-dfg-234f -c mychannel
+python emailServer.py -c my_config.json
+```
+
+##Configuration
+Configuration is done through a JSON config file whose location is passed on the cmd line.
+Below is a description of its overall structure:
+```json
+{
+  "aws_access_key": "<IAM user's key>",
+  "aws_secret_key": "<IAM user's secret key>",
+  "s3_bucket": "<Your S3 bucket to store images>",
+  "slack_key": "<Your bot's slack API key>",
+  "channel": "<The slack channel to post messages to>",
+  "port": <The TCP port number that the SMTP server will listen on>
+}
 ```
--- emailServer.py
+++ emailServer.py
@@ -6,21 +6,31 @@ from slacker import Slacker
 import tempfile
 import base64
 import argparse
+import boto
+from boto.s3.key import Key
+import uuid
+import PIL
+from PIL import Image
+from cStringIO import StringIO
+
+# Init json parsing...
+import json
 
 parser = argparse.ArgumentParser(description='Posts first image attachment of emails to a slack channel')
 
-parser.add_argument('-c', metavar='<channel name>', dest='channel', help='The channel to post messages to',
-                    required=True)
-parser.add_argument('-p', metavar='<smtp port>', dest='port',
-                    help='The port that the SMTP server should listen on (defaults to 2500)',
-                    type=int, default=2500, required=False)
-parser.add_argument('-s', metavar='<slack api key>', dest='slack_key', help='Your Slack bot''s API key',
+parser.add_argument('-c', metavar='<config file path>', dest='config', help='The location of your config file',
                     required=True)
 
 args = parser.parse_args()
 
-slack = Slacker(args.slack_key)
+# Load up the config from our JSON file...
+config = {}
+with open(args.config, 'r') as f:
+    config = json.load(f)
+
+slack = Slacker(config['slack_key'])
 inbox = Inbox()
+
 
 @inbox.collate
 def handle(to, sender, subject, body):
@@ -41,16 +51,61 @@ def handle(to, sender, subject, body):
             image_contents = part.get_payload().replace("\n", "")
 
     if len(image_contents) == 0:
-        slack.chat.post_message(args.channel, subject)
+        slack.chat.post_message(config['channel'], subject)
     else:
+        resize_and_upload(image_contents, subject, msg_txt)
+
+
+def resize_and_upload(base_64_contents, subject, msg):
+    temp = None
+    image_contents = base64.b64decode(base_64_contents)
+    key = uuid.uuid4().hex
+    full_url = None
+    thumb_url = None
+
+    try:
         temp = tempfile.NamedTemporaryFile(delete=False)
-        try:
-            temp.write(base64.b64decode(image_contents))
-            temp.flush()
-            slack.files.upload(temp.name, title=subject, channels=args.channel,
-                               filename='img.jpg', initial_comment=msg_txt)
-        finally:
-            temp.close()
+        temp.write(image_contents)
+        temp.flush()
+        full_url = upload_to_s3(temp, 'full', key)
+
+        # slack.files.upload(temp.name, title=subject, channels=config['channel'],
+        #                   filename='img.jpg', initial_comment=msg_txt)
+    finally:
+        temp.close()
+
+    try:
+        temp = tempfile.NamedTemporaryFile(delete=False)
+
+        file_imgdata = StringIO(image_contents)
+        image = Image.open(file_imgdata)
+        image.thumbnail((300, 300), PIL.Image.ANTIALIAS)
+        image.save(temp.name, 'PNG')
+
+        thumb_url = upload_to_s3(temp, 'thumb', key)
+
+        # slack.files.upload(temp.name, title=subject, channels=config['channel'],
+        #                   filename='img.jpg', initial_comment=msg_txt)
+    finally:
+        temp.close()
+
+    slack.chat.post_message(config['channel'], msg.strip(), as_user='Camera Bot', username='camerabot',
+                            attachments=[{
+                                'title': subject.strip(),
+                                'title_link': full_url,
+                                'image_url': thumb_url
+                            }]
+                            )
+
+
+def upload_to_s3(file, folder, key):
+    conn = boto.connect_s3(config['aws_access_key'], config['aws_secret_key'])
+    bucket = conn.get_bucket(config['s3_bucket'])
+    k = Key(bucket)
+    k.storage_class = 'STANDARD_IA'
+    k.key = folder + '/' + key
+    k.set_contents_from_filename(file.name, headers={'Content-Type': 'image/jpeg'})
+    return 'https://s3.amazonaws.com/' + config['s3_bucket'] + '/' + k.name
 
 
 def collect_sub_messages(msg, msgs):
@@ -63,12 +118,13 @@ def collect_sub_messages(msg, msgs):
 
     return msgs
 
+
 print """
 #
-#   Binding SMTP to Slack Relay on port """ + str(args.port) + """
-#   Posting to slack channel '""" + str(args.channel) + """'
+#   Binding SMTP to Slack Relay on port """ + str(config['port']) + """
+#   Posting to slack channel '""" + str(config['channel']) + """'
 #
 """
 
 # Bind directly.
-inbox.serve(address='0.0.0.0', port=args.port)
+inbox.serve(address='0.0.0.0', port=config['port'])
